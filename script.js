9ee7473787e10ced1bc25b5dad7848ed1a1329a0
anis <boulilaa247@gmail.com>
2024-01-19 06:40:37 +0100

    fix the problem

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 const myButton = document.getElementById("myButton");
 const myLable = document.getElementById("myLable");
 
-// let max = document.getElementById("max").value;
-// let min = document.getElementById("min").value;
-
-// let max = window.prompt("enter a max number");
-// let min = window.prompt("enter a min number");
-
-// max = Number(max);
-// min = Number(min);
-
-const max = 100;
-const min = 0;
 
 let randomNum;
+let max;
+let min;
 
 myButton.onclick = function () {
-  randomNum = Math.floor(Math.random() * max) + min;
+  max = document.getElementById("max").value;
+  min = document.getElementById("min").value;
+  max = Number(max);
+  min = Number(min);
+  randomNum = Math.floor(Math.random() * (max + 1 - min)) + min;
   myLable.textContent = randomNum;
 };
